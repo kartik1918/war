94839b68e0c72d23ca96878a64fb33179696480b
Kartik Gupta <kartikgupta966@gmail.com>
2021-12-19 13:25:15 +0530

    added core functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let deckId;
-let deckImage = document.getElementById('card-img');
+const cardsContainer = document.getElementById('cards')
 
 function newDeck() {
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
@@ -22,10 +22,31 @@ document.getElementById('draw-cards').addEventListener('click', function() {
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            let image = ''
-            for (let i = 0; i < data.cards.length; i++) {
-                image += `<img src='${data.cards[i].image}' />`
-            }
-            deckImage.innerHTML = image;
+            cardsContainer.children[0].innerHTML = `<img src='${data.cards[0].image}' class="card"/>'`
+            cardsContainer.children[1].innerHTML = `<img src='${data.cards[1].image}' class="card"/>'`
         })
 })
+
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value);
+    const card2ValueIndex = valueOptions.indexOf(card2.value);
+    if (card1ValueIndex > card2ValueIndex) {
+        console.log('Card1 Win')
+    } else if (card1ValueIndex < card2ValueIndex) {
+        console.log('Card2 Win')
+    } else {
+        console.log("It's a tie")
+    }
+}
+
+const card1Obj = {
+    value: "ACE"
+}
+
+const card2Obj = {
+    value: "ACE"
+}
+
+determineCardWinner(card1Obj, card2Obj)
